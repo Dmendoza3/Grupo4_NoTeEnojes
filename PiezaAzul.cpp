b1286f50d44529714f0b375ab7ce9025eafbd68e
yoshuadupin <djosue@unitec.edu>
2017-03-10 17:09:15 -0600

    corrigiendo

--- PiezaAzul.cpp
+++ PiezaAzul.cpp
@@ -5,16 +5,16 @@ PiezaAzul::PiezaAzul(char** tablero):Pieza(14 , 14 , 'a'){
 	this->ingresarTablero(tablero);
 }
 
-virtual void PiezaAzul::ingresarTablero(char** tablero){
+void PiezaAzul::ingresarTablero(char** tablero){
 	tablero[14][14] = this->getColor();
 
 }
-virtual void PiezaAzul::ingresarZonaFinal(char** tablero){
+void PiezaAzul::ingresarZonaFinal(char** tablero){
 	if(tablero[this->getX()][this->getY()]=='A'){
 		this->esZonaFinal();
 	}
 }
-virtual void PiezaAzul::comer(char**){
+void PiezaAzul::comer(char**){
 	
 }
 
